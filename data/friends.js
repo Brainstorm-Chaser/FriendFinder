06f10532f31a5aac4d74957911043ec107cfd38e
Mike Murphy <mike@brainstormchaser.com>
2017-10-26 21:53:42 -0700

    updated routes

--- data/friends.js
+++ data/friends.js
@@ -4,7 +4,7 @@ var friendsArray = [
  {
   friendName : "Janet",
   friendPhoto : "http://cdn.apk-cloud.com/detail/screenshot/WDyYh0NRdpj9AFG6ZoHghzSNhTqPjDEH0Vn_l-5LtBDsG4vQhAsVrShWbyfKqXs1eVs=h900.png",
-  answers : [2,4]
+  // answers : [2,4]
  }
 ];
 console.log(friendsArray)
--- routing/apiroutes.js
+++ routing/apiroutes.js
@@ -2,12 +2,21 @@ var friendData = require("../data/friends");
 
 module.exports = function(app){
 
-  app.get("/survey"), function(req,res){
-    res.json(friends)
+//  app.get("/api/new", function (req, res){
+//    res.json(friends)
+
+// });
+
+  app.post("/api/new", function(req,res){
+    // console.log(req.body)
+    friends.push(req.body);
+    // res.json(req.body);
+ });
+
   }
 
   // app.post("/api/friends", function(req,res){
   //   friends.push(req.body);
   //   // res.json(true);
   //  });
-  }
+ 
